73d98b159c97f586d702abf2d6a2d1c8d8986f7e
Austin <austin.puthen@gmail.com>
2021-12-19 17:30:41 -0500

    adding scanfile.java

---
+++ src/ScanFile1.java
@@ -0,0 +1,29 @@
+import java.io.File;
+import java.util.Scanner;
+
+class ScanFile1{
+    public static void main(String [] args) throws Exception {
+
+        File f = new File("src/testfile.txt");
+        Scanner scan = new Scanner(f);
+
+        int wordCount = 0;
+
+        while (scan.hasNextLine()){
+//split the spaces:
+            String s = scan.nextLine();
+            String [] s_arr = s.split(" ");
+
+            for(int i=0;i<s_arr.length ; i++){
+                for(int j=0;j<s_arr.length ; j++){
+                    if(s_arr[j].equals(s_arr[i]))
+                        wordCount++;
+                }
+                System.out.println(s_arr[i] + "-" + wordCount);
+            }
+
+        }
+
+    }
+
+}
